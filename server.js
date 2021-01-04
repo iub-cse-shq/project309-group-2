139b10ba495338520f69c557dc6810cf1ea65be5
Junayed7166 <junayed.ndc16@gmail.com>
2021-01-04 23:04:11 +0600

    01/JANUARY/2021

--- server.js
+++ server.js
@@ -2,25 +2,26 @@ var http = require('http')
 var express = require('express')
 var app = express()
 var server = http.Server(app)
+app.use(express.static('public'))
 
 // your server routes go here
 
 //index or main page
 app.get('/', function (request, response) {
     //console.log(request)
-    response.sendFile(__dirname + '/index.html')
+    response.sendFile(__dirname + '/client/index.html')
 })
 
 //Course Name and Code page/ course page
 app.get('/CourseNameAndCode', function (request, response) {
     //console.log(request)
-    response.sendFile(__dirname + '/CourseNameAndCode.html')
+    response.sendFile(__dirname + '/client/CourseNameAndCode.html')
 })
 
 //Routine Page
 app.get('/Routine', function (request, response) {
     //console.log(request)
-    response.sendFile(__dirname + '/Routine.html')
+    response.sendFile(__dirname + '/client/Routine.html')
 })
 
 server.listen(process.env.PORT || 3000, 
